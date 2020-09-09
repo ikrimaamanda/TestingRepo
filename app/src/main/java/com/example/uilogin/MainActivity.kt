9c4828504499e0cb900d4c4418dc3583535430a9
ikrimaamanda <ikrima.amanda.07@gmail.com>
2020-09-09 20:01:33 +0700

    ini adalah ui login

---
+++ app/src/main/java/com/example/uilogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uilogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
